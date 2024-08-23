6bff737d1c1bbdff3caa74f7c9bd2199824c6d6f
SamuelPaz10 <111510648+SamuelPaz10@users.noreply.github.com>
2024-08-22 20:50:50 -0600

    Creación de migraciones y modelos.

---
+++ database/migrations/2024_08_23_023536_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion', 100);
+            $table->double('precio', 14,2);
+            $table->integer('stock');
+            $table->boolean('pagaIsv');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
